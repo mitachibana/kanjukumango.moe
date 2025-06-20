b619dc64ac5d2e7aa775d96692c44c521d3f4b69
スィッケルス　マイケルジョージ / MICHAEL GEORGE SICKELS <michael.sickels@mimaki.com>
2025-06-20 11:21:36 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
